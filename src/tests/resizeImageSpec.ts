7746ffe6410b6fce3af68bd7a60e23e6ec63b54b
Abdulaziz m1 <aziz4000@gmail.com>
2022-10-21 23:12:55 +0300

    Implement endpoint testcase
    Comment the other test case because it achieves the sane functionality (If needed it should be uncommented)

--- src/tests/resizeImageSpec.ts
+++ src/tests/resizeImageSpec.ts
@@ -1,11 +1,18 @@
-import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
+import supertest from 'supertest';
+import express from 'express';
+import resizeImage from '../routes/api/resizeImage';
+// For the second commented test case
+// import axios from 'axios';
 
 const dirname = __dirname;
+const app = express();
 
-// Make sure the server is running on localhost port 3000
+app.get('/api/images', resizeImage.resizeImageAPI);
+
 it('expect to resize image to 300 w and 350 h from endpoint', async () => {
+
   const filename = 'palmtunnel';
   const width = 300;
   const height = 350;
@@ -14,9 +21,26 @@ it('expect to resize image to 300 w and 350 h from endpoint', async () => {
     `${dirname}/../../output/${filename}_${width}_${height}.jpg`
   );
 
-  await axios.get(
-    `http://localhost:3000/api/images?filename=${filename}&width=${width}&height=${height}`
-  );
+  await supertest(app)
+    .get(`/api/images?filename=${filename}&width=${width}&height=${height}`)
+    .expect(200)
 
-  expect(fs.existsSync(outputFilePath)).toBeTrue();
+    expect(fs.existsSync(outputFilePath)).toBeTrue();
 });
+
+// // Uncomment to run this test that requires the server to be running on port 3000
+// it('expect to resize image to 300 w and 350 h from endpoint', async () => {
+//   const filename = 'palmtunnel';
+//   const width = 300;
+//   const height = 350;
+
+//   const outputFilePath = path.resolve(
+//     `${dirname}/../../output/${filename}_${width}_${height}.jpg`
+//   );
+
+//   await axios.get(
+//     `http://localhost:3000/api/images?filename=${filename}&width=${width}&height=${height}`
+//   );
+
+//   expect(fs.existsSync(outputFilePath)).toBeTrue();
+// });
