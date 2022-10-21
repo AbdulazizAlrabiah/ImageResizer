2e0fe1a743106b8896ed77da5b11a65f37628a10
Abdulaziz m1 <aziz4000@gmail.com>
2022-10-21 23:24:49 +0300

    Lint test file

--- src/tests/resizeImageSpec.ts
+++ src/tests/resizeImageSpec.ts
@@ -12,7 +12,6 @@ const app = express();
 app.get('/api/images', resizeImage.resizeImageAPI);
 
 it('expect to resize image to 300 w and 350 h from endpoint', async () => {
-
   const filename = 'palmtunnel';
   const width = 300;
   const height = 350;
@@ -23,9 +22,9 @@ it('expect to resize image to 300 w and 350 h from endpoint', async () => {
 
   await supertest(app)
     .get(`/api/images?filename=${filename}&width=${width}&height=${height}`)
-    .expect(200)
+    .expect(200);
 
-    expect(fs.existsSync(outputFilePath)).toBeTrue();
+  expect(fs.existsSync(outputFilePath)).toBeTrue();
 });
 
 // // Uncomment to run this test that requires the server to be running on port 3000
